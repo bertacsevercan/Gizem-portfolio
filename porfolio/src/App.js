d2337a5de686cd9591bf43454a97b25749e15ccc
bertacsevercan <bertac.severcan@gmail.com>
2020-12-13 23:35:45 +0300

    create i18n and add app inside

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import Resume from "./containers/Resume";
+import Contact from "./containers/Contact";
+import Projects from "./containers/Projects";
+// import NavBar from "./components/NavBar";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+// import Footer from "./components/Footer";
+// import ReactGA from "react-ga";
+// import { useEffect } from "react";
 
 function App() {
   return (
+    <I18nextProvider i18n={i18n}>
     <div className="App">
-      
+      <div className="content-wrapper">
+        <Router>
+        {/*   <NavBar /> */}
+          <Route exact path="/" component={Home} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+       {/*    <Footer /> */}
+        </Router>
+      </div>
     </div>
+  </I18nextProvider>
   );
 }
 
