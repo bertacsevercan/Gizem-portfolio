834129b1c8eed905144444a497ea3ca5725fbe42
bertacsevercan <bertac.severcan@gmail.com>
2020-12-14 01:47:58 +0300

    add sidNav dummy

--- porfolio/src/components/SideNav/index.jsx
+++ porfolio/src/components/SideNav/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const SideNav = () => {
   return (
     <div className="sideNav">
       <h1>LOGO</h1>
+      <h1>TR/EN</h1>
+      <h1>{<Link to="/">Home</Link>}</h1>
+      <h1>{<Link to="/resume">Resume</Link>}</h1>
+      <h1>{<Link to="/projects">Projects</Link>}</h1>
+      <h1>{<Link to="/contact">Contact</Link>}</h1>
     </div>
   );
 };
