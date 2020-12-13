912be0164064a210bae64e28f17b723cf71206ca
bertacsevercan <bertac.severcan@gmail.com>
2020-12-13 23:50:34 +0300

    create siders

---
+++ porfolio/src/components/RightSider/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RightSider = () => {
+    return (
+        <div className="rightSider">
+
+        </div>
+
+    );
+
+}
+
+export default RightSider;
---
+++ porfolio/src/components/SideNav/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideNav = () => {
+    return (
+        <div className="sideNav">
+
+        </div>
+
+    );
+
+}
+
+export default SideNav;
