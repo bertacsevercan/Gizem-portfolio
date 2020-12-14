f3e8273ebb67500e5687b2c30442b6ebb1bfb8b3
bertacsevercan <bertac.severcan@gmail.com>
2020-12-14 14:41:06 +0300

    set active links style

--- porfolio/src/components/SideNav/index.jsx
+++ porfolio/src/components/SideNav/index.jsx
@@ -1,19 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 const SideNav = () => {
+  // const [xPosition, setX] = useState(-)
+
+  /*   useEffect(()=> {
+    setX(0);
+  }, []); */
+
   return (
     <div className="sideNav">
       <div id="logo">
         <h1>LOGO</h1>
       </div>
       <div className="navLinks">
-      <h1>TR/EN</h1>
-      <h1>{<Link to="/">Home</Link>}</h1>
-      <h1>{<Link to="/resume">Resume</Link>}</h1>
-      <h1>{<Link to="/projects">Projects</Link>}</h1>
-      <h1>{<Link to="/contact">Contact</Link>}</h1>
+        <h1>TR/EN</h1>
+        <h1>
+          {
+            <NavLink activeStyle={{ color: "grey" }} exact to="/">
+              Home
+            </NavLink>
+          }
+        </h1>
+        <h1>
+          {
+            <NavLink activeStyle={{ color: "grey" }} to="/resume">
+              Resume
+            </NavLink>
+          }
+        </h1>
+        <h1>
+          {
+            <NavLink activeStyle={{ color: "grey" }} to="/projects">
+              Projects
+            </NavLink>
+          }
+        </h1>
+        <h1>
+          {
+            <NavLink activeStyle={{ color: "grey" }} to="/contact">
+              Contact
+            </NavLink>
+          }
+        </h1>
       </div>
     </div>
   );
