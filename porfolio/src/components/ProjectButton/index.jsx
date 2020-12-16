9cc2592daf5c1d72623b39ef01cf795d2f7e4930
bertacsevercan <bertac.severcan@gmail.com>
2020-12-16 12:59:16 +0300

    add hover animate to button

--- porfolio/src/components/ProjectButton/index.jsx
+++ porfolio/src/components/ProjectButton/index.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const ProjectButton = ({ color }) => {
+  const [nameVisible, setNameVisible] = useState("none");
+  const [arrowVisible, setArrowVisible] = useState("block");
+
+  const showElements = () => {
+    setNameVisible("block");
+    setArrowVisible("none");
+  };
+
+  const hideElements = () => {
+    setNameVisible("none");
+    setArrowVisible("block");
+  };
+
   return (
-    <div className="projectButton" style={{ backgroundColor: color }}>
-      <h1 id="arrow">&#10095;</h1>
+    <div
+      onMouseLeave={hideElements}
+      onMouseEnter={showElements}
+      className="projectButton"
+      style={{ backgroundColor: color }}
+    >
+      <button id="button">
+        <p style={{ display: nameVisible }} id="projectName">
+          Project Name
+        </p>
+        <h1 style={{ display: arrowVisible }} id="arrow">
+          &#10095;
+        </h1>
+      </button>
     </div>
   );
 };
