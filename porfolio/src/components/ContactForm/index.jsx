5a01d19026e9df27a238c938fcc09a4b814c0875
bertacsevercan <bertac.severcan@gmail.com>
2020-12-19 23:57:08 +0300

    add contact form

---
+++ porfolio/src/components/ContactForm/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FaLevelDownAlt } from "react-icons/fa";
+import "./style.css";
+
+const ContactForm = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="contact">
+      <h1>
+        {t("contact.titles.title0")}
+        <FaLevelDownAlt />{" "}
+      </h1>
+      <Form name="contact">
+        <input type="hidden" name="form-name" value="contact" />
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>{t("contact.labels.label0")}</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            required="true"
+            placeholder={t("contact.placeholders.holder0")}
+          />
+        </Form.Group>
+        <Form.Group controlId="formInputName">
+          <Form.Label>{t("contact.labels.label1")}</Form.Label>
+          <Form.Control
+            name="fullName"
+            placeholder={t("contact.placeholders.holder1")}
+            required="true"
+          />
+        </Form.Group>
+        <Form.Group controlId="formInputSubject">
+          <Form.Label>{t("contact.labels.label2")}</Form.Label>
+          <Form.Control
+            name="subject"
+            placeholder={t("contact.placeholders.holder2")}
+            required="true"
+          />
+        </Form.Group>
+        <Form.Group controlId="form.ControlTextarea">
+          <Form.Label>{t("contact.labels.label3")}</Form.Label>
+          <Form.Control
+            name="message"
+            required="true"
+            placeholder={t("contact.placeholders.holder3")}
+            as="textarea"
+            rows={3}
+          />
+        </Form.Group>
+        <Button variant="danger" type="submit" block>
+          {t("contact.button")}
+        </Button>
+      </Form>
+    </div>
+  );
+};
+export default ContactForm;
--- porfolio/src/containers/Contact/index.jsx
+++ porfolio/src/containers/Contact/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ContactForm from "../../components/ContactForm";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <h1>Contact</h1>
+      <ContactForm />
     </div>
   );
 };
