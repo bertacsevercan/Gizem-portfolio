4c29f92281c9465f977491c8788ace72aef98e23
bertacsevercan <bertac.severcan@gmail.com>
2020-12-20 17:35:35 +0300

    add home page img and text

--- porfolio/src/containers/Home/index.jsx
+++ porfolio/src/containers/Home/index.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import { useTranslation } from "react-i18next";
+import "./style.css";
 
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="home">
-      <h1>HOME</h1>
+      <Image
+        width="50%"
+        src="https://image.freepik.com/free-vector/architect-concept-illustration_114360-3042.jpg"
+        roundedCircle
+      />
+      <h1>{t("home.head")}</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+        quis numquam consequuntur nobis quos fuga eius incidunt adipisci ea,
+        vero officiis soluta, similique odio molestiae corporis iusto optio cum
+        accusantium?
+      </p>
     </div>
   );
 };
