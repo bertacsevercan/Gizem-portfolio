169b7384e32de9554c25a0c2f0ebbca617c916ff
bertacsevercan <bertac.severcan@gmail.com>
2020-12-20 21:29:50 +0300

    add resume placeholders

--- porfolio/src/containers/Resume/index.jsx
+++ porfolio/src/containers/Resume/index.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import "./style.css";
 
 const Resume = () => {
   return (
     <div className="resume">
-      <h1>Resume</h1>
+      <div className="text-wrapper">
+        <p>
+          <b>Employment History</b>
+        </p>
+        <p>
+          <b>Education</b>
+        </p>
+      </div>
+      <div className="img-text-wrapper">
+        <Image
+          id="profilePic"
+          width="30%"
+          src="https://image.freepik.com/free-vector/architect-concept-illustration_114360-3042.jpg"
+          thumbnail
+        />
+        <p>
+          <b>Technical Skills</b>
+        </p>
+        <p>
+          <b>Languages</b>
+        </p>
+      </div>
     </div>
   );
 };
