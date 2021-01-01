2cb4d3467ae687218171db606c8e8579916e9138
bertacsevercan <bertac.severcan@gmail.com>
2021-01-01 15:31:52 +0300

    add new image

--- porfolio/src/CarouselPictures/index.jsx
+++ porfolio/src/CarouselPictures/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import test from "../images/test.png";
+import test2 from "../images/test2.jpeg";
 
 const images = [
   [
@@ -12,7 +13,7 @@ const images = [
     <img
       height="550px"
       className="d-block w-100"
-      src="https://image.freepik.com/free-vector/flat-design-house-collection_23-2148683215.jpg"
+      src={test2}
       alt="Second slide"
     />,
     <img
