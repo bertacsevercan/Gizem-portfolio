a3fd66bec1669d0035c46ef473d2f050fd02589e
bertacsevercan <bertac.severcan@gmail.com>
2021-01-26 15:08:13 +0300

    add grid to home

--- porfolio/src/containers/Home/index.jsx
+++ porfolio/src/containers/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import { useTranslation } from "react-i18next";
 import "./style.css";
@@ -8,20 +9,30 @@ const Home = () => {
 
   return (
     <div className="home">
-      <Image
-        width="50%"
-        src="https://image.freepik.com/free-vector/architect-concept-illustration_114360-3042.jpg"
-        roundedCircle
-      />
-      <div className="homeText">
-        <h1>{t("home.head")}</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-          quis numquam consequuntur nobis quos fuga eius incidunt adipisci ea,
-          vero officiis soluta, similique odio molestiae corporis iusto optio
-          cum accusantium?
-        </p>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Image
+              width="50%"
+              src="https://image.freepik.com/free-vector/architect-concept-illustration_114360-3042.jpg"
+              roundedCircle
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="homeText">
+              <h1>{t("home.head")}</h1>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Blanditiis quis numquam consequuntur nobis quos fuga eius
+                incidunt adipisci ea, vero officiis soluta, similique odio
+                molestiae corporis iusto optio cum accusantium?
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
