93a05436b71d2e31b878f9f4f1c008bcf6880330
bertacsevercan <bertac.severcan@gmail.com>
2021-03-08 01:49:08 +0300

    remove unused imports

--- porfolio/src/containers/Projects/index.jsx
+++ porfolio/src/containers/Projects/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectGallery from "../ProjectGallery";
 import * as assets from "../../assets";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./style.css";
 
 const projectProps = [];
